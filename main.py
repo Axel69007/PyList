191ce325e42ece0ac39b71087d11e19608fe1c43
Axel <ingrao.axel.al@gmail.com>
2025-04-26 18:08:25 +0200

    Ajout du module fichier

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 #Pylist V1
 #Axel INGRAO
 #GPL Licence
+from datetime import datetime
 #Functions 
-def module_affichage():
+def Module_affichage():
     print("Bienvenue dans Pylist")
     print("Entrez ce que vous voulez faire :")
     print("1 = Ajoutez une tâche")
     print("2 = Voir les tâches")
     print("3 = supprimer une tâches")
     print("4 = Sortie")
+def Module_fichier(Liste_Saved):
+    Date_AJD=datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    Nom_fichier=str(Date_AJD)+".txt"
+    with open(Nom_fichier,"w") as fichier:
+        fichier.write(Liste_Saved)
 #Déclation des variables
 list = []
-#appel affichage
-module_affichage()
+#Appel fonction
+Module_affichage()
 #Boucle infinie du programme
 while True :
     Choix=(input("Entrez votre choix : "))
@@ -28,6 +34,7 @@ while True :
             Choix_suppression=input("Entrez l'index de la tâches à supprimer : ")
             list.remove(list[int(Choix_suppression)])
         case"4":
+            Module_fichier(str(list))
             break
         case _:
             print("Choix invalide...")
